81abf0a46a8af420eb7c63800f21ee6928016c17
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2021-08-14 11:39:49 +0530

    ✔ Footer section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./style/style.css";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import ScrollUp from "./components/ScrollUp";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <Navbar />
       <Main />
+      <Footer />
       <ScrollUp />
     </>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>Crafted by JD.</p>
+            <p>Data is provided from newsapi.org, covid2019-api.</p>
+        </footer>
+    )
+}
+
+export default Footer;
